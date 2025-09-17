950e30f49ddcc7092a981e9d5d04d7e0a5a8429d
squalldai <squall.dai@gmail.com>
2025-09-17 14:54:53 +0800

    Update sensient.js

--- sensient.js
+++ sensient.js
@@ -1,1 +1,12 @@
 
+// sensient.js
+// 预留森鑫算法
+
+const SensientAlgorithm = {
+  encrypt: function(left, right) {
+    return "[森鑫算法未实现] 输入:" + left + "+" + right;
+  },
+  decrypt: function(left, right) {
+    return "[森鑫算法未实现] 输入:" + left + "+" + right;
+  }
+};
